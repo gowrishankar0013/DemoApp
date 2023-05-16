4926ade288a82d02b12dc8dc979bd0bed0b6fe44
gowrishankar0013 <gowrishankar.0013@gmail.com>
2023-05-16 21:17:32 +0530

    Deleted all

--- src/components/demo/FooterComponent.jsx
+++
@@ -1,9 +0,0 @@
-function FooterComponent() {
-    return (
-        <footer className="footer">
-            <span className="text-muted">All Rights Reserved 2023 @Shankar</span>
-        </footer>
-    )
-}
-
-export default FooterComponent
